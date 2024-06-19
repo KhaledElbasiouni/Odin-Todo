3c2fbadaa6366c62ea9f2e3a5b28e96b1889043a
KhaledElbasiouni <khaled.elbasiouni222@gmail.com>
2024-06-19 09:01:34 -0400

    Add editing project name

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const addProjectsBtn = document.querySelector("#add-projects-btn");
 //Global variables
 let newProjectInputFieldCreated = false;
 let newProjectInputField = undefined;
-let projects = [];
+let projects = {};
 
 // Event listeners
 addProjectsBtn.addEventListener("click", createNewProjectInputField);
@@ -28,7 +28,7 @@ function createNewProjectInputField() {
     return;
   }
 
-  newProjectInputField = createProjectInputElement(newProjectInputField);
+  newProjectInputField = createProjectInputElement();
 
   projectsContainer.appendChild(newProjectInputField);
   newProjectInputField.focus();
@@ -43,7 +43,7 @@ function abortCreateNewProject(event) {
   removeEventListener(event.target, "focusout", abortCreateNewProject);
 }
 
-function createProjectInputElement(value = undefined) {
+function createProjectInputElement({ value = undefined, id = undefined } = {}) {
   let inputElement = document.createElement("input");
   attachEventListener(inputElement, "focusout", inputFieldFocusOut);
   inputElement.type = "text";
@@ -52,6 +52,9 @@ function createProjectInputElement(value = undefined) {
 
   if (value) {
     inputElement.value = value;
+  }
+  if (id) {
+    inputElement.dataset.id = id;
   }
   return inputElement;
 }
@@ -64,28 +67,46 @@ function removeEventListener(node, event, listener) {
   node.removeEventListener(event, listener);
 }
 
-// Can this function be made more generic?
 function inputFieldFocusOut(event) {
   removeEventListener(event.target, "focusout", inputFieldFocusOut);
   let input = event.target.value;
-
+  let projectId = event.target.dataset.id;
   event.target.parentNode.removeChild(event.target);
 
   if (input !== "") {
-    addNewProject(input);
+    if (projectId) {
+      editExistingProject(projectId, input);
+    } else {
+      addNewProject(input);
+    }
   }
 }
 
 function addNewProject(projectName) {
-  let { projectContainerDiv, editProjectBtn } = createNewProjectComponent(projectName);
+  let projectId = createNewProjectObject(projectName).id;
+
+  let { projectContainerDiv, editProjectBtn } = createNewProjectComponent(
+    projectName,
+    projectId
+  );
 
   projectsContainer.appendChild(projectContainerDiv);
   attachEventListener(editProjectBtn, "click", editProjectActive);
 }
 
-function createNewProjectComponent(projectName) {
+function editExistingProject(id, newName) {
+  let projectObj = changeProjectObjectName(id, newName);
+
+  let { projectContainerDiv, editProjectBtn } = createNewProjectComponent(newName, id);
+
+  projectsContainer.appendChild(projectContainerDiv);
+  attachEventListener(editProjectBtn, "click", editProjectActive);
+}
+
+function createNewProjectComponent(projectName, projectId) {
   let projectContainerDiv = document.createElement("div");
   projectContainerDiv.classList.add("project-container");
+  projectContainerDiv.dataset.id = projectId;
 
   let projectTitle = document.createElement("div");
   projectTitle.classList.add("project-title");
@@ -104,7 +125,23 @@ function createNewProjectComponent(projectName) {
 function editProjectActive(event) {
   let projectContainerDiv = event.target.parentNode.parentNode;
   let projectName = projectContainerDiv.firstChild.innerText;
-  let projectInputField = createProjectInputElement(projectName);
+  let projectInputField = createProjectInputElement({
+    value: projectName,
+    id: projectContainerDiv.dataset.id,
+  });
 
   projectsContainer.replaceChild(projectInputField, projectContainerDiv);
+}
+
+function createNewProjectObject(projectName) {
+  let newProject = new Project(projectName);
+  projects[newProject.id] = newProject;
+
+  return newProject;
+}
+
+function changeProjectObjectName(id, newName) {
+  projects[id].name = newName;
+
+  return projects[id];
 }
